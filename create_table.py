ad217deb27ebdef6f07eeba62454c0586dd5b16e
Madhan M <54719274+CaptMadhan@users.noreply.github.com>
2022-10-03 16:39:29 +0530

    Added snapshots and updated create_table.py

--- create_table.py
+++ create_table.py
@@ -1,3 +1,10 @@
+'''
+    create_table.py file is responsible to create table "Data" and "Backup_Data" if it did not exists.
+
+    This file is run only in the initial stage i.e. when the project is run for the first time.
+    "Data" table is used to store all the values needed for the project run, and is accessed when needed.
+    "Backup_Data" table is used as a backup wherein no read or write operation is done, to secure from processing error. 
+'''
 import sqlite3 as base
 
 # create or connect a data base
@@ -6,7 +13,20 @@ data_base = base.connect("demo1.db")
 # create a cursor
 cursor = data_base.cursor()
 
-cursor.execute('''create table IF NOT EXISTS FACTORY(
+cursor.execute('''create table IF NOT EXISTS Data(
 ELEMENTS INT
 );''')
-#cursor.execute("DROP TABLE FACTORY")
+
+cursor.execute('''create table IF NOT EXISTS Backup_Data(
+ELEMENTS INT
+);''')
+cursor.execute('''SELECT distinct tbl_name from sqlite_master order by 1;''')
+for row in cursor.fetchall():
+    print(row)
+#cursor.execute('''SELECT * from Data''')
+#for row in cursor.fetchall():
+#    print(row)
+cursor.execute('''SELECT * from Backup_Data''')
+for row in cursor.fetchall():
+    print(row)
+#cursor.execute("DROP TABLE Data")
